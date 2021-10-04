c1e8f5eecaf80c7b0092213e136d3b1ff7ed1b8e
KumarB95 <kumarbalingal@gmail.com>
2021-10-04 22:43:40 +0530

    Refactored : Validating First name min 3 char and First char is Capital

--- src/main/java/userRegistrationProblem.java
+++ src/main/java/userRegistrationProblem.java
@@ -5,11 +5,10 @@ import java.util.regex.Pattern;
 public class userRegistrationProblem {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter your Name : ");
-        String name= sc.nextLine();
-        Pattern pattern=Pattern.compile("[A-Z]{1}[a-z]{3,20}$");
-        Matcher matcher=pattern.matcher(name);
-        System.out.println(matcher.find());
+
+        System.out.println("Enter your First Name : ");
+        String firstName= sc.nextLine();
+        System.out.println(Pattern.matches("[A-Z][a-z]{2,}",firstName));
 
     }
 }
