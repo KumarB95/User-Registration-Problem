c362dc78a2dabd8628dade4fbb03fb13d904caf4
KumarB95 <kumarbalingal@gmail.com>
2021-10-18 13:00:38 +0530

    Refactored : Code is refactored to handle exception for null input

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -12,28 +12,71 @@ public class UserRegistration {
         System.out.println("Welcome to User Registration Validation Problem");
     }
 
-    public boolean validFirstName(String fname) {
-        Pattern pattern = Pattern.compile(NAME_PATTERN);
-        return pattern.matcher(fname).matches();
+    public boolean validFirstName(String fname) throws UserRegistrationException {
+        try {
+            if (fname.length() == 0)
+                throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_EMPTY, "First name can not be empty");
+            Pattern pattern = Pattern.compile(NAME_PATTERN);
+            return pattern.matcher(fname).matches();
+        } catch (NullPointerException e) {
+            throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_NULL, "First name cannot be null");
+        }
     }
 
-    public boolean validateLastName(String lname) {
-        Pattern pattern = Pattern.compile(NAME_PATTERN);
-        return pattern.matcher(lname).matches();
+    /**
+     * Purpose : To Validate Last Name Of User
+     */
+    public boolean validateLastName(String lname) throws UserRegistrationException {
+        try {
+            if (lname.length() == 0)
+                throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_EMPTY, "Last name can not be empty");
+            Pattern pattern = Pattern.compile(NAME_PATTERN);
+            return pattern.matcher(lname).matches();
+        } catch (NullPointerException e) {
+            throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_NULL, "Last name cannot be null");
+        }
     }
 
-    public boolean validateEmailAddress(String mail) {
-        Pattern pattern = Pattern.compile(EMAIL_ADDRESS_PATTERN);
-        return pattern.matcher(mail).matches();
+    /**
+     * Purpose : To Validate The Email Address Of User
+     */
+    public boolean validateEmailAddress(String mail) throws UserRegistrationException {
+        try {
+            if (mail.length() == 0)
+                throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_EMPTY, "E-Mail address can not be empty");
+            Pattern pattern = Pattern.compile(EMAIL_ADDRESS_PATTERN);
+            return pattern.matcher(mail).matches();
+        } catch (NullPointerException e) {
+            throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_NULL, "E-Mail address can not be null");
+        }
     }
 
-    public boolean validateMobileNumber(String phoneNumber) {
-        Pattern pattern = Pattern.compile(MOBILE_NUMBER);
-        return pattern.matcher(phoneNumber).matches();
+    /**
+     * Purpose : To Validate The Mobile Number Of User
+     */
+    public boolean validateMobileNumber(String phoneNumber) throws UserRegistrationException {
+        try {
+            if (phoneNumber.length() == 0)
+                throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_EMPTY, "Mobile number can not be empty");
+            Pattern pattern = Pattern.compile(MOBILE_NUMBER);
+            return pattern.matcher(phoneNumber).matches();
+        } catch (NullPointerException e) {
+            throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_NULL, "Mobile number can not be null");
+        }
     }
 
-    public boolean validatePassword(String password) {
-        Pattern pattern = Pattern.compile(PASSWORD);
-        return pattern.matcher(password).matches();
+    /**
+     * Purpose : To Validate The Password Of User
+     */
+    public boolean validatePassword(String password) throws UserRegistrationException {
+        try {
+            if (password.length() == 0)
+                throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_EMPTY, "Password can not be empty");
+            Pattern pattern = Pattern.compile(PASSWORD);
+            return pattern.matcher(password).matches();
+        } catch (NullPointerException e) {
+            throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_NULL, "Password can not be null");
+        }
+
     }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,163 +1,374 @@
 package com.bridgelabz;
 import com.bridgelabz.UserRegistration;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 //import org.testng.Assert;
 
 import java.util.Scanner;
 
 public class UserRegistrationTest {
-    //Scanner sc=new Scanner(System.in);
     @Test
-    public void whenFirstNameEnteredFirstCharMustBeCapsAndAtLeast3Char(){
-        UserRegistration validator =new UserRegistration();
-        boolean result=validator.validFirstName("Navalkumar");
-        Assert.assertTrue(result);
+    void givenFirstName_WhenProper_ShouldReturnTrue() {
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validFirstName("Naval");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertTrue(result);
     }
+
     @Test
     void givenFirstName_WhenShort_ShouldReturnFalse() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validFirstName("Na");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validFirstName("Na");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertFalse(result);
     }
 
     @Test
     void givenFirstName_WhenWithNumberOrSpecialCharacters_ShouldReturnFalse() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validFirstName("Nav&al");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validFirstName("N@val");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertFalse(result);
     }
 
     @Test
     void givenLastName_WhenProper_ShouldReturnTrue() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validateLastName("Balingal");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validateLastName("Balingal");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertTrue(result);
     }
 
     @Test
     void givenLastName_WhenShort_ShouldReturnFalse() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validateLastName("Ba");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validateLastName("Ba");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertFalse(result);
     }
 
     @Test
     void givenLastName_WhenWithNumberOrSpecialCharacters_ShouldReturnFalse() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validateLastName("Bal%ingal");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validateLastName("B@alingal");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertFalse(result);
     }
 
     @Test
     void givenLastName_WhenStartingWithSmallLetter_ShouldReturnFalse() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validateLastName("balingal");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validateLastName("balingal");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertFalse(result);
     }
 
     @Test
     void givenEmail_WhenValid_ShouldReturnTrue() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validateEmailAddress("kumarbalingal@gmail.com");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validateEmailAddress("kumarb@gmail.com");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertTrue(result);
     }
 
     @Test
     void givenEmail_WhenUserNameIsMissing_ShouldReturnFalse() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validateEmailAddress("@gmail.com");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validateEmailAddress("@gmail.com");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertFalse(result);
     }
 
     @Test
     void givenEmail_WhenDomainNameIsMissing_ShouldReturnFalse() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validateEmailAddress("kumarb370@.com");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validateEmailAddress("kumarb@.com");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertFalse(result);
     }
 
     @Test
     void givenEmail_WhenMainTldIsMissing_ShouldReturnFalse() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validateEmailAddress("kumarbalingal@gmail");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validateEmailAddress("kumarb@gmail");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertFalse(result);
     }
 
     @Test
     void givenEmail_WhenAtTheRateIsMissing_ShouldReturnFalse() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validateEmailAddress("kumarbgmail.com");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validateEmailAddress("kumarbgmail.com");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertFalse(result);
     }
 
     @Test
     void givenEmail_WhenWithOptionalTld_ShouldReturnTrue() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validateEmailAddress("kumarb@gmail.co.in");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validateEmailAddress("kumarb@gmail.co.in");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertTrue(result);
     }
 
     @Test
     void givenMobileNumber_WhenValid_ShouldReturnTrue() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validateMobileNumber("91 8055055582");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validateMobileNumber("91 8055055582");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertTrue(result);
     }
 
     @Test
     void givenMobileNumber_WhenNoCountryCode_ShouldReturnFalse() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validateMobileNumber("8055055582");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validateMobileNumber("8055055582");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertFalse(result);
     }
 
     @Test
     void givenMobileNumber_WhenSpaceIsMissingAfterCountryCode_ShouldReturnFalse() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validateMobileNumber("918055055582");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validateMobileNumber("918055055582");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertFalse(result);
     }
 
     @Test
     void givenMobileNumber_WhenShort_ShouldReturnFalse() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validateMobileNumber("91 805505");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validateMobileNumber("91 80550");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertFalse(result);
     }
 
     @Test
     void givenPassword_WhenValid_ShouldReturnTrue() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validatePassword("Navalkuamr@2201");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validatePassword("Naval@2201");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertTrue(result);
     }
 
     @Test
     void givenPassword_WhenShort_ShouldReturnFalse() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validatePassword("Naval12");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validatePassword("Naval12");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertFalse(result);
     }
 
     @Test
     void givenPassword_WhenWithAtLeastOneUpperCase_ShouldReturnTrue() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validatePassword("naval@2201");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validatePassword("naval@2201");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertTrue(result);
     }
 
     @Test
     void givenPassword_WhenWithAtLeastOneNumericNumber_ShouldReturnTrue() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validatePassword("Naval&balingal");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validatePassword("Naval&b95");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertTrue(result);
     }
 
     @Test
     void givenPassword_WhenWithAtLeastOneSpecialCharacters_ShouldReturnTrue() {
-        UserRegistration validator =new UserRegistration();
-        boolean result = validator.validatePassword("Naval2021");
-        Assert.assertTrue(result);
+        UserRegistration validator = new UserRegistration();
+        boolean result = false;
+        try {
+            result = validator.validatePassword("Naval2201");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void givenFirstName_WhenNull_ShouldThrow_UserRegistrationException() {
+        UserRegistration validator = new UserRegistration();
+        try {
+            validator.validFirstName(null);
+        } catch (UserRegistrationException e) {
+            Assertions.assertEquals(UserRegistrationException.ExceptionType.ENTERED_NULL, e.type);
+        }
+    }
+
+    @Test
+    void givenFirstName_WhenEmpty_ShouldThrow_UserRegistrationException() {
+        UserRegistration validator = new UserRegistration();
+        try {
+            validator.validFirstName("");
+        } catch (UserRegistrationException e) {
+            Assertions.assertEquals(UserRegistrationException.ExceptionType.ENTERED_EMPTY, e.type);
+        }
+    }
+
+    @Test
+    void givenLastName_WhenNull_ShouldThrow_UserRegistrationException() {
+        UserRegistration validator = new UserRegistration();
+        try {
+            validator.validateLastName(null);
+        } catch (UserRegistrationException e) {
+            Assertions.assertEquals(UserRegistrationException.ExceptionType.ENTERED_NULL, e.type);
+        }
+    }
+
+    @Test
+    void givenLastName_WhenEmpty_ShouldThrow_UserRegistrationException() {
+        UserRegistration validator = new UserRegistration();
+        try {
+            validator.validateLastName("");
+        } catch (UserRegistrationException e) {
+            Assertions.assertEquals(UserRegistrationException.ExceptionType.ENTERED_EMPTY, e.type);
+        }
+    }
+
+    @Test
+    void givenEmailAddress_WhenNull_ShouldThrow_UserRegistrationException() {
+        UserRegistration validator = new UserRegistration();
+        try {
+            validator.validateEmailAddress(null);
+        } catch (UserRegistrationException e) {
+            Assertions.assertEquals(UserRegistrationException.ExceptionType.ENTERED_NULL, e.type);
+        }
+    }
+
+    @Test
+    void givenEmailAddress_WhenEmpty_ShouldThrow_UserRegistrationException() {
+        UserRegistration validator = new UserRegistration();
+        try {
+            validator.validateEmailAddress("");
+        } catch (UserRegistrationException e) {
+            Assertions.assertEquals(UserRegistrationException.ExceptionType.ENTERED_EMPTY, e.type);
+        }
+    }
+
+    @Test
+    void givenMobileNumber_WhenNull_ShouldThrow_UserRegistrationException() {
+        UserRegistration validator = new UserRegistration();
+        try {
+            validator.validateMobileNumber(null);
+        } catch (UserRegistrationException e) {
+            Assertions.assertEquals(UserRegistrationException.ExceptionType.ENTERED_NULL, e.type);
+        }
+    }
+
+    @Test
+    void givenMobileNumber_WhenEmpty_ShouldThrow_UserRegistrationException() {
+        UserRegistration validator = new UserRegistration();
+        try {
+            validator.validateMobileNumber("");
+        } catch (UserRegistrationException e) {
+            Assertions.assertEquals(UserRegistrationException.ExceptionType.ENTERED_EMPTY, e.type);
+        }
+    }
+
+    @Test
+    void givenPassword_WhenNull_ShouldThrow_UserRegistrationException() {
+        UserRegistration validator = new UserRegistration();
+        try {
+            validator.validatePassword(null);
+        } catch (UserRegistrationException e) {
+            Assertions.assertEquals(UserRegistrationException.ExceptionType.ENTERED_NULL, e.type);
+        }
+    }
+
+    @Test
+    void givenPassword_WhenEmpty_ShouldThrow_UserRegistrationException() {
+        UserRegistration validator = new UserRegistration();
+        try {
+            validator.validatePassword("");
+        } catch (UserRegistrationException e) {
+            Assertions.assertEquals(UserRegistrationException.ExceptionType.ENTERED_EMPTY, e.type);
+        }
     }
 }
