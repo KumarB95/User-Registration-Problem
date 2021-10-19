0bdf98c212d9a6b5a76798e856121b9a12cd87a5
KumarB95 <kumarbalingal@gmail.com>
2021-10-20 00:24:10 +0530

    Refactored : Code is refactored to valid user inputs using lambda function

---
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class UserRegistrationMain {
+    public static void main(String[] args) {
+        UserRegistrationLambdaExpression userRegistration = new UserRegistrationLambdaExpression();
+        userRegistration.validateUserDetails();
+    }
+}
